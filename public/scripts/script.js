8ef9429626093883ade90a5a113e5fe29e698fd3
JustinLung <justinlung77@gmail.com>
2022-06-08 09:00:54 +0200

    fixed some bugs and made some various changes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,27 @@
 // Variables
-
 let socket = io()
+
+// User List Variables
 let userButton = document.querySelector('#user-button')
 let closeButton = document.querySelector('.close-button')
 let usersList = document.querySelector('.users-list')
-let userCountEl = document.querySelector('.user-count')
 let overlay = document.querySelector('.overlay')
+
+// Messages Variables
 let messages = document.querySelector('#message-container')
 let loadingMessages = document.querySelector('#loading-messages')
 let messageInput = document.querySelector('#message-input')
 let messageForm = document.querySelector('form')
 let submitButton = document.querySelector('.submit-button')
-let name = localStorage.getItem('name') || prompt('What is your name?')
 let isTyping = document.querySelector('#is-typing')
 let date = new Date().toLocaleDateString('en-gb')
 
+let name = localStorage.getItem('name') || prompt('What is your name?')
+
 // Eventlisteners and Function Decleration
+localStorage.setItem('name', name)
+
+renderWelcomeMessage()
 
 userButton.addEventListener('click', () => {
   overlay.classList.add('open-menu')
@@ -27,44 +33,12 @@ closeButton.addEventListener('click', () => {
 
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  if (messageInput.value === '') return
-  isTyping.innerHTML = ''
   let message = messageInput.value
-  loadingMessages.insertAdjacentHTML(
-    'beforeend',
-    `
-    <li class="loading-message">
-      <span class="circle loading">${name.charAt(0)}</span>
-      <div class="message">
-        <h2>${name} ${date} <span><img class="loading-image" src="assets/icons/clock.svg" /> Sending...</span></h2>
-        <p>${message}</p>
-      </div>
-    </li>
-  `
-  )
-  setTimeout(() => {
-    loadingMessages.innerHTML = ''
-    messages.insertAdjacentHTML(
-      'beforeend',
-      `
-      <li>
-        <span class="circle">${name.charAt(0)}</span>
-        <div class="message">
-          <h2>${name} ${date}</h2>
-          <p>${message}</p>
-        </div>
-      </li>
-    `
-    )
-  }, 500)
+  renderSendMessage()
   socket.emit('send-chat-message', message)
   messageInput.value = ''
   messages.scrollTo(0, messages.scrollHeight)
 })
-
-localStorage.setItem('name', name)
-
-renderWelcomeMessage()
 
 messageInput.addEventListener('keypress', function () {
   socket.emit('typing', name)
@@ -88,7 +62,7 @@ socket.on('chat-message', (data) => {
     'beforeend',
     `
     <li class="loading-message">
-      <span class="circle-loading">${name.charAt(0)}</span>
+      <span class="circle loading">${name.charAt(0)}</span>
       <div class="message">
         <h2>${data.name} ${date}</h2>
         <p>${data.message}</p>
@@ -136,4 +110,37 @@ function renderWelcomeMessage() {
     `beforeend`,
     `<li class="welcome-message">Welcome ${name}, have a great time talking to everybody!</li>`
   )
+}
+
+function renderSendMessage() {
+  if (messageInput.value === '') return
+  isTyping.innerHTML = ''
+  let message = messageInput.value
+  loadingMessages.insertAdjacentHTML(
+    'beforeend',
+    `
+    <li class="loading-message">
+      <span class="circle loading">${name.charAt(0)}</span>
+      <div class="message">
+        <h2>${name} ${date} <span><img class="loading-image" src="assets/icons/clock.svg" /> Sending...</span></h2>
+        <p>${message}</p>
+      </div>
+    </li>
+  `
+  )
+  setTimeout(() => {
+    loadingMessages.innerHTML = ''
+    messages.insertAdjacentHTML(
+      'beforeend',
+      `
+      <li>
+        <span class="circle">${name.charAt(0)}</span>
+        <div class="message">
+          <h2>${name} ${date}</h2>
+          <p>${message}</p>
+        </div>
+      </li>
+    `
+    )
+  }, 500)
 }
